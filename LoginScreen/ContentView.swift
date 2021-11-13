58aa1b90a1ffb811696b9e678360077ea163fecd
Code Palace <codepalace@fastmail.se>
2021-11-13 15:15:03 +0100

    Initial Commit

---
+++ LoginScreen/ContentView.swift
@@ -0,0 +1,86 @@
+//
+//  ContentView.swift
+//  LoginScreen
+//
+//  Created by Federico on 13/11/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var username = "mario2021"
+    @State private var password = "mario123"
+    @State private var wrongUsername: Float = 0
+    @State private var wrongPassword: Float  = 0
+    @State private var showingLoginScreen = false
+    
+    
+    var body: some View {
+        NavigationView {
+            ZStack {
+                Color.blue
+                    .ignoresSafeArea()
+                Circle()
+                    .scale(1.7)
+                    .foregroundColor(.white.opacity(0.15))
+                Circle()
+                    .scale(1.35)
+                    .foregroundColor(.white)
+
+                VStack {
+                    Text("Login")
+                        .font(.largeTitle)
+                        .bold()
+                        .padding()
+                    
+                    TextField("Username", text: $username)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                        .border(.red, width: CGFloat(wrongUsername))
+                        
+                    
+                    SecureField("Password", text: $password)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                        .border(.red, width: CGFloat(wrongPassword))
+                    
+                    Button("Login") {
+                        authenticateUser(username: username, password: password)
+                        }
+                    .foregroundColor(.white)
+                    .frame(width: 300, height: 50)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+                    
+                    NavigationLink(destination: Text("You are logged in @\(username)"), isActive: $showingLoginScreen) {
+                        EmptyView()
+                    }
+                }
+            }.navigationBarHidden(true)
+        }
+    }
+    
+    func authenticateUser(username: String, password: String) {
+        if username.lowercased() == "mario2021" {
+            wrongUsername = 0
+            if password.lowercased() == "abc123" {
+                wrongPassword = 0
+                showingLoginScreen = true
+            } else {
+                wrongPassword = 2
+            }
+        } else {
+            wrongUsername = 2
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LoginScreen/LoginScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginScreenApp.swift
+//  LoginScreen
+//
+//  Created by Federico on 13/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct LoginScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
